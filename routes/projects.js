e211eb2df7d79e3fabfeda5df0bd2cc3e78d6960
Parker Abegg <pbabegg@gmail.com>
2020-03-10 21:35:47 -0600

    Second commit

--- routes/projects.js
+++ routes/projects.js
@@ -40,6 +40,7 @@ router.post("/", (req, res, next) =>{
 });
 
 router.delete("/:id", (req, res, next) =>{
+  console.log(req.params.id)
   Project.findOneAndDelete({_id: req.params.id},
     function(err, data){
       if(err){
